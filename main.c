315008127c64b179e2141f47637c4f80407746e8
Danilo Araujo Duleba <danduleba@gmail.com>
2026-09-06 20:06:30 -0300

    inicio da funação de carregar arquivo

--- main.c
+++ main.c
@@ -34,3 +34,32 @@ int ler_positivo(const char *texto, int *valor){
     *valor = (int)numero;
     return 1;
 }
+
+int carregar_arquivo(const char *nome_arquivo, int *tempo_total, Tarefa **tarefas, int *quantidade){
+    FILE *arquivo;
+    char linha[256];
+    char texto_periodo[32];
+    char texto_deadline[32];
+    char texto_burst[32];
+    char extra[2];
+    Tarefa tarefa;
+    Tarefa *novo;
+
+    arquivo = fopen(nome_arquivo, "r");
+
+    if(arquivo==NULL){
+        printf(stderr,"Não foi possivel abrir o arquivo\n");
+        return 0;
+    }
+    if(fgets(linha, sizeof linha, arquivo) == NULL || !ler_positivo(linha, tempo_total)){
+        fprintf(stderr, "tempo total invalido\n");
+        return 0;
+    }
+    *tarefas = NULL;
+    *quantidade = 0;
+
+    while (fgets(linha,sizeof linha, arquivo)!= NULL){ 
+        if(sscanf(linha,  "%31s %31s %31s %31s %31s"))
+    }
+    
+}
